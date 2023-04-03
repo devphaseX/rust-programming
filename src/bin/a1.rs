4381a1ee75f3fe0796f4704c543db4d0244b8aef
Ayomide <ayomidelawal700@gmail.com>
2023-04-03 09:30:07 +0100

    create a function to print name

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,13 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    display_name();
+}
+
+fn display_name() {
+    let first_name = "Ayomide";
+    let last_name = "Lawal";
+
+    println!("{} {}", first_name, last_name);
+}
