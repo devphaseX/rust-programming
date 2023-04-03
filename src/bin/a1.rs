4aa2e007228a08ea7699cb0e5e50c9c981dadfa9
Ayomide <ayomidelawal700@gmail.com>
2023-04-03 09:50:23 +0100

    encapulate name print to individual fns

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -4,17 +4,19 @@
 // * Displays your first and last name
 //
 // Notes:
-// * Use a function to display your first name
-// * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
 fn main() {
-    let first_name = "Ayomide";
-    let last_name = "Lawal";
-    display_name(&first_name);
-    display_name(&last_name);
+    display_firstname();
+    display_lastname();
 }
 
-fn display_name(name: &str) {
-    println!("{}", name);
+// * Use a function to display your last name
+fn display_firstname() {
+    println!("Ayomide");
+}
+
+// * Use a function to display your first name
+fn display_lastname() {
+    println!("Lawal")
 }
