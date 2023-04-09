1585389701f276349d34841e081fe5eb7249bc96
Ayomide <ayomidelawal700@gmail.com>
2023-04-09 08:47:22 +0100

    enum color example

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -10,4 +10,31 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+#![allow(dead_code)]
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+fn main() {
+    let color = Color::Red;
+    print_color(&color);
+    println!("{:?}", color);
+}
+
+fn print_color(color: &Color) {
+    match color {
+        Color::Blue => {
+            println!("na blue")
+        }
+
+        Color::Green => {
+            println!("na green")
+        }
+
+        Color::Red => {
+            println!("na red")
+        }
+    }
+}
