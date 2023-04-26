28d320c84513bd95b541c6162b53017e69b17e0b
Ayomide <ayomidelawal700@gmail.com>
2023-04-26 11:32:20 +0100

    practice the tuple a9

--- src/bin/a9.rs
+++ src/bin/a9.rs
@@ -9,4 +9,18 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn main() {
+    let (_, y) = get_coord();
+
+    if y > 5 {
+        println!("The y coord is greater than 5")
+    } else if y < 5 {
+        println!("The y coord is less than 5")
+    } else {
+        println!("The y coord is on 5 dot.")
+    }
+}
+
+fn get_coord() -> (i32, i32) {
+    (4, 0)
+}
