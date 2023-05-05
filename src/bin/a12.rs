a82842b8da09cf7759d55a74a9c4d2cfb7139ae7
Ayomide <ayomidelawal700@gmail.com>
2023-05-05 16:38:21 +0100

    complete activity a12

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -9,5 +9,72 @@
 // * Use an enum for the box color
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
+#![allow(dead_code)]
+fn main() {
+    let box_item = Box::new(Dimensions::new(7.0, 8.0, 4.0), 345, BoxColour::GREEN);
+    box_item.print();
+}
 
-fn main() {}
+#[derive(Debug)]
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn new(width: f64, height: f64, depth: f64) -> Self {
+        Self {
+            width,
+            height,
+            depth,
+        }
+    }
+    fn print(&self) {
+        println!("width {:?}", self.width);
+        println!("height {:?}", self.height);
+        println!("depth {:?}", self.depth);
+    }
+}
+
+#[derive(Debug)]
+
+struct Box {
+    dimension: Dimensions,
+    weight: u32,
+    colour: BoxColour,
+}
+
+impl Box {
+    fn new(dimension: Dimensions, weight: u32, colour: BoxColour) -> Self {
+        Self {
+            dimension,
+            weight,
+            colour,
+        }
+    }
+
+    fn print(&self) {
+        self.colour.print();
+        self.dimension.print();
+        println!("weight {:?}", self.weight);
+    }
+}
+
+#[derive(Debug)]
+
+enum BoxColour {
+    RED,
+    BLUE,
+    GREEN,
+}
+
+impl BoxColour {
+    fn print(&self) {
+        match self {
+            Self::GREEN => println!("green"),
+            Self::BLUE => println!("blue"),
+            Self::RED => println!("red"),
+        }
+    }
+}
